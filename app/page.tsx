007ffe326db69b5a3eacb8e5b7f8dfdb44841457
Kumayl Lokhandwala <kumayl.lokhandwala2022@vitstudent.ac.in>
2026-02-18 01:31:44 +0530

    deployement fix

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,9 @@ export default function Home() {
   const login = async () => {
     await supabase.auth.signInWithOAuth({
       provider: "google",
+      options: {
+        redirectTo: `${window.location.origin}/dashboard`,
+      },
     });
   };
 
